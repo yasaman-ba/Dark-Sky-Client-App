df40f32b5fc9afcc782bbca3f2d4a63990556d2e
yasamanba <yasamanbaghparvar@gmail.com>
2018-05-31 19:44:13 +0430

    this is Dark sky client App folders

---
+++ DarkSkyClient/app/src/androidTest/java/com/darkskyclient/yas_b/darkskyclient/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.darkskyclient.yas_b.darkskyclient;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.darkskyclient.yas_b.darkskyclient", appContext.getPackageName());
+    }
+}
